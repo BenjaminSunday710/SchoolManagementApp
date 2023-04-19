bcc478e7b5ed44789202804b0b02db1cdffa4e4f
Sunday Benjamin <benjaminsunday710@gmail.com>
2023-04-19 08:54:53 +0100

    renamed folder

--- UserManagement.Application/Queries/Roles/FetchRolePermissions/FetchRolePermissionsQueryHandler.cs
+++ UserManagement.Application/Queries/Roles/FetchRolePermissions/FetchRolePermissionsQueryHandler.cs
@@ -1,25 +1,28 @@
-﻿using Shared.Application.ArchitectureBuilder.Queries;
+﻿using NHibernate.Util;
+using Shared.Application.ArchitectureBuilder.Queries;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using UserManagement.Application.Queries.SharedResponses;
-using UserManagement.Domain.Roles;
+using UserManagement.Domain.Permissions;
 using Utilities.Result.Util;
 
 namespace UserManagement.Application.Queries.Roles.FetchRolePermissions
 {
-    public class FetchRolePermissionsQueryHandler : QueryHandler<Role, Guid, List<PermissionResponse>, FetchRolePermissionsQuery>
+    public class FetchRolePermissionsQueryHandler : QueryHandler<Permission, Guid, List<PermissionResponse>, FetchRolePermissionsQuery>
     {
         public override async Task<ActionResult<List<PermissionResponse>>> HandleAsync(FetchRolePermissionsQuery query, CancellationToken cancellationToken = default)
         {
-            var role = await QueryContext.GetByIdAsync(query.RoleId);
+            var permissions = await QueryContext.FindAsync(permission => permission.Roles.Any(role => role.Id == query.RoleId));
             var responses = new List<PermissionResponse>();
-            foreach (var permission in role.Permissions)
+            foreach (var permission in permissions)
             {
                 responses.Add(new PermissionResponse()
                 {
-                    Name = permission.Name
+                    Name = permission.Name,
+                    Id=permission.Id
                 });
             }
             return OperationResult.Successful(responses);

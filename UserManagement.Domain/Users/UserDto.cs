56e9b7d66e05b5b3c587a255d5a6623f98e8e4c5
Sunday Benjamin <benjaminsunday710@gmail.com>
2023-04-19 07:56:06 +0100

    added user dto

---
+++ UserManagement.Domain/Users/UserDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserManagement.Domain.Users
+{
+    public class UserDto
+    {
+        public  Guid UserId { get; set; }
+        public  string FirstName { get; set; }
+        public  string LastName { get; set; }
+        public  string Email { get; set; }
+    }
+}

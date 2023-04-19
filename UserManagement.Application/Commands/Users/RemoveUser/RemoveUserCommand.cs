04b2e25c079cf7b2c3a7282b87cdf68ae85d9d38
Sunday Benjamin <benjaminsunday710@gmail.com>
2023-04-19 08:03:22 +0100

    handled remove user command

---
+++ UserManagement.Application/Commands/Users/RemoveUser/RemoveUserCommand.cs
@@ -0,0 +1,19 @@
+﻿using Shared.Application.ArchitectureBuilder.Commands;
+using System;
+using Utilities.Result.Util;
+using Utilities.Validations;
+
+namespace UserManagement.Application.Commands.Users.RemoveUser
+{
+    public class RemoveUserCommand : Command
+    {
+        protected override ActionResult Validate()
+        {
+            return new FluentValidator()
+                .IsValidGuid(UserId, $"{UserId} is invalid user id")
+                .Result;
+        }
+
+        public Guid UserId { get; set; }
+    }
+}
---
+++ UserManagement.Application/Commands/Users/RemoveUser/RemoveUserCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using Shared.Application.ArchitectureBuilder.Commands;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using UserManagement.Infrastructure.Context;
+using Utilities.Result.Util;
+
+namespace UserManagement.Application.Commands.Users.RemoveUser
+{
+    public class RemoveUserCommandHandler : CommandHandler<RemoveUserCommand, UserManagementDbContext, CommandResponse>
+    {
+        public override async Task<ActionResult<CommandResponse>> HandleAsync(RemoveUserCommand command, CancellationToken cancellationToken = default)
+        {
+            var user = await Context.UserRepository.GetByIdAsync(command.UserId);
+
+            if (user == null) return OperationResult.Failed($"User with id:{command.UserId} not found");
+
+            var response=new CommandResponse(user.Id);
+
+            return OperationResult.Successful(response);
+        }
+    }
+}

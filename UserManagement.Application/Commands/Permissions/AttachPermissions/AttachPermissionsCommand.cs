237a204856b6c834a05c6096c7280163cff01001
Sunday Benjamin <benjaminsunday710@gmail.com>
2023-04-19 07:58:01 +0100

    handled attach permissions command

---
+++ UserManagement.Application/Commands/Permissions/AttachPermissions/AttachPermissionsCommand.cs
@@ -0,0 +1,22 @@
+﻿using Shared.Application.ArchitectureBuilder.Commands;
+using System;
+using System.Collections.Generic;
+using Utilities.Result.Util;
+using Utilities.Validations;
+
+namespace UserManagement.Application.Commands.Permissions.AttachPermissions
+{
+    public class AttachPermissionsCommand : Command
+    {
+        protected override ActionResult Validate()
+        {
+            return new FluentValidator()
+                .IsValidGuid(RoleId, $"{RoleId} is an invalid Id")
+                .IsValidCollection(PermissionIds, $"{PermissionIds} is invalid permission Ids")
+                .Result;
+        }
+
+        public Guid RoleId { get; set; }
+        public IEnumerable<Guid> PermissionIds { get; set; }
+    }
+}

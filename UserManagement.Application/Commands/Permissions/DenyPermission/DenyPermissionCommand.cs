af9113598697a010288471ba93c04ceca1599346
Sunday Benjamin <benjaminsunday710@gmail.com>
2023-04-19 07:58:52 +0100

    handled deny permission command

---
+++ UserManagement.Application/Commands/Permissions/DenyPermission/DenyPermissionCommand.cs
@@ -0,0 +1,21 @@
+﻿using Shared.Application.ArchitectureBuilder.Commands;
+using System;
+using Utilities.Result.Util;
+using Utilities.Validations;
+
+namespace UserManagement.Application.Commands.Permissions.DenyPermission
+{
+    public class DenyPermissionCommand : Command
+    {
+        protected override ActionResult Validate()
+        {
+            return new FluentValidator()
+                .IsValidGuid(RoleId, $"{RoleId} is invalid role id")
+                .IsValidGuid(PermissionId, $"{PermissionId} is invalid permission id")
+                .Result;
+        }
+
+        public Guid RoleId { get; set; }
+        public Guid PermissionId { get; set; }
+    }
+}
---
+++ UserManagement.Application/Commands/Permissions/DenyPermission/DenyPermissionCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Shared.Application.ArchitectureBuilder.Commands;
+using System.Threading;
+using System.Threading.Tasks;
+using UserManagement.Infrastructure.Context;
+using Utilities.Result.Util;
+
+namespace UserManagement.Application.Commands.Permissions.DenyPermission
+{
+    public class DenyPermissionCommandHandler : CommandHandler<DenyPermissionCommand, UserManagementDbContext, CommandResponse>
+    {
+        public override async Task<ActionResult<CommandResponse>> HandleAsync(DenyPermissionCommand command, CancellationToken cancellationToken = default)
+        {
+            var role = await Context.RoleRepository.GetByIdAsync(command.RoleId);
+
+            if (role == null) return OperationResult.Failed($"Role with id:{command.RoleId} not found");
+
+            var permission = await Context.PermissionRepository.GetByIdAsync(command.PermissionId);
+
+            role.DenyPermission(permission);
+            var response = new CommandResponse(role.Id);
+
+            await Context.RoleRepository.UpdateAsync(role,command.RoleId);
+            var commitStatus = await Context.CommitAsync();
+
+            return commitStatus.WasSuccessful ? OperationResult.Successful(response) : OperationResult.Failed("Unable to deny permission");
+        }
+    }
+}

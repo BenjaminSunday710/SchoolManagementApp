c803853d1ec58b50148c45e290ede0232ca34e39
Sunday Benjamin <benjaminsunday710@gmail.com>
2023-04-19 08:00:36 +0100

    handled resign role command

---
+++ UserManagement.Application/Commands/Roles/ResignRole/ResignRoleCommand.cs
@@ -0,0 +1,21 @@
+﻿using Shared.Application.ArchitectureBuilder.Commands;
+using System;
+using Utilities.Result.Util;
+using Utilities.Validations;
+
+namespace UserManagement.Application.Commands.Roles.ResignRole
+{
+    public class ResignRoleCommand : Command
+    {
+        protected override ActionResult Validate()
+        {
+            return new FluentValidator()
+                .IsValidGuid(UserId, $"{UserId} is invalid user id")
+                .IsValidGuid(RoleId, $"{RoleId} is invalid role id")
+                .Result;
+        }
+
+        public Guid UserId { get; set; }
+        public Guid RoleId { get; set; }
+    }
+}
---
+++ UserManagement.Application/Commands/Roles/ResignRole/ResignRoleCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using Shared.Application.ArchitectureBuilder.Commands;
+using System.Threading;
+using System.Threading.Tasks;
+using UserManagement.Infrastructure.Context;
+using Utilities.Result.Util;
+
+namespace UserManagement.Application.Commands.Roles.ResignRole
+{
+    public class ResignRoleCommandHandler : CommandHandler<ResignRoleCommand, UserManagementDbContext, CommandResponse>
+    {
+        public override async Task<ActionResult<CommandResponse>> HandleAsync(ResignRoleCommand command, CancellationToken cancellationToken = default)
+        {
+            var user = await Context.UserRepository.GetByIdAsync(command.UserId);
+            if (user == null) return OperationResult.Failed($"User with id:{command.UserId} not found");
+
+            var role = await Context.RoleRepository.GetByIdAsync(command.RoleId);
+            if (role == null) return OperationResult.Failed($"Role with id:{command.RoleId} not found");
+
+            user.ResignRole(role);
+
+            await Context.UserRepository.UpdateAsync(user, command.UserId);
+            var response = new CommandResponse(user.Id);
+
+            var commitStatus = await Context.CommitAsync();
+
+            return commitStatus.WasSuccessful ? OperationResult.Successful(response) : OperationResult.Failed("Unable to resign role");
+        }
+    }
+}
